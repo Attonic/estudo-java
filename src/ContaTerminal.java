fbd5d01747087d86cddc81579de1fe936303f1a0
attonic <a.cleisonn@gmail.com>
2024-11-19 22:59:12 -0300

    Adcionei alguns comentarios

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,15 @@
+/**
+ * Atividade referente a Desavio proposto pela DIO.
+ * @author Antonio Cleison
+ * @version 1.0
+ *
+ * */
 import java.util.Locale;
 import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
+
+        //Cria um Scanner para ler as entradas do usuário
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor digite o numero da conta: ");
@@ -17,7 +25,7 @@ public class ContaTerminal {
 
         System.out.println("Qual seu saldo: ");
         float saldo = scanner.nextFloat();
-
+        //Fecha o Scanner utilizado
           scanner.close();
 
         System.out.println("Olá " + nomeCliente + " Obrigado por criar uma conta na nosso banco,\n sua agência é: " + agencia + " conta: " + numeroConta + "\ne seu saldo " + saldo + " já esta disponível para saque");
